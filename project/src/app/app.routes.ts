3d2ae900a5747b10e3a4e51b5411d944ec495f54
Elvio Barbosa <elviobarbosa@gmail.com>
2024-06-16 20:02:33 -0300

    feat: nada novo

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -2,13 +2,13 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
-    { 
-        path: 'cadastro', 
-        loadComponent: () =>
-            import(
-              "./register/register.component"
-            ).then((c) => c.RegisterComponent),
-    }
+  { 
+    path: 'cadastro', 
+    loadComponent: () =>
+      import(
+        "./register/register.component"
+      ).then((c) => c.RegisterComponent),
+  }
 ];
 
 
